35d5bb28477a6e1c3f4f32ec16ebe5daf6fee6cb
BeerManNotAvailable2 <Siamionrychihin@yandex.by>
2025-04-20 17:01:23 +0300

    upload startup project

---
+++ weatherapplication/app/src/main/java/com/example/weatherapplication/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.weatherapplication
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.example.weatherapplication.databinding.ActivityMainBinding
+import com.example.weatherapplication.fragments.MainFragment
+import org.json.JSONObject
+
+const val  API_KEY = "c8d6ee88c18b4ae5b0793432252004"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.placeHolder, MainFragment.newInstance()).commit()
+    }
+}
---
+++ weatherapplication/app/src/main/java/com/example/weatherapplication/fragments/MainFragment.kt
@@ -0,0 +1,51 @@
+package com.example.weatherapplication.fragments
+
+import android.Manifest
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.weatherapplication.R
+import com.example.weatherapplication.databinding.ActivityMainBinding
+import com.example.weatherapplication.databinding.FragmentMainBinding
+
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+    private lateinit var pLauncher: ActivityResultLauncher<String>
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        checkPermission()
+    }
+
+    private fun permissionListener(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            Toast.makeText(activity, "Permission is $it", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    private fun checkPermission(){
+        if(!isPermissionGranted(Manifest.permission.ACCESS_FINE_LOCATION)){
+            permissionListener()
+            pLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = MainFragment()
+    }
+}
